74d729bd4f587a0a37a8dcdf1eafb836ea2018dc
Saeed <saeedhajhussein189@gmail.com>
2026-05-26 17:12:42 +0200

    3rd time trying

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
           اعثر على الخبير المناسب لمنزلك
         </h1>
 
-        <p className="text-balance text-sm text-gray-200">
+        <p className="text-balance text-sm text-gray-400">
           تواصل مع نجارين وسباكين وكهربائيين وغيرهم من ذوي الخبرة الموثوق بهم
           ببضعة نقرات
         </p>
